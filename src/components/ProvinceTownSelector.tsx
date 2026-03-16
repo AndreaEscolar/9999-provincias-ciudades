b5bc221dcb8a08a6391b58d9bb952b0d6b4fa501
Andrea Escolar <andrea.escolar.artigas@ibadia.cat>
2026-03-16 15:47:37 +0100

    Añade estado para la ciudad seleccionada

--- src/components/ProvinceTownSelector.tsx
+++ src/components/ProvinceTownSelector.tsx
@@ -5,29 +5,58 @@ import type { AutonomousCommunity } from "../types/location"
 export default function ProvinceTownSelector () {
 
     const [selectedProvince, setSelectedProvince] = useState<string>("")
+    const [selectedTown, setSelectedTown] = useState<string>("")
 
     const data = arbol as AutonomousCommunity[]
     const provinces = data.flatMap((community) => community.provinces)
+    
     const currentProvince = provinces.find(
         (province) => province.label === selectedProvince
     )
 
+    const availableTowns = currentProvince ? currentProvince.towns : []
+
+    console.log("selectedProvince:", selectedProvince)
+    console.log("currentProvince:", currentProvince)
+    console.log("availableTowns:", availableTowns)
+
     function handleProvinceChange(event: React.ChangeEvent<HTMLSelectElement>) {
         setSelectedProvince(event.target.value)
+        setSelectedTown("")
     }
 
-    console.log(currentProvince)
+    function handleTownChange(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSelectedTown(event.target.value)
+    }
+    
     return (
         <section>
             <label htmlFor="province">Provincia</label>
             <select id="province" value={selectedProvince} onChange={handleProvinceChange}>
                 <option value=""> Escoge una provincia</option>
 
-                {provinces.map((province) =>
+                {provinces.map((province) => (
                     <option key={province.code} value={province.label}>
                         {province.label}
                     </option>
-                )}
+                ))}
+            </select>
+
+            <label htmlFor="town">Ciudad</label>
+            <select 
+                key={selectedProvince}
+                id="town" 
+                value={selectedTown} 
+                onChange={handleTownChange} 
+                disabled={!selectedProvince}
+            >
+                <option value=""> Escoge una ciudad</option>
+
+                {availableTowns.map((town) => (
+                    <option key={town.code} value={town.label}>
+                        {town.label}
+                    </option>
+                ))}
             </select>
         </section>
     )
