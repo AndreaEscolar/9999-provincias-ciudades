ea4384339e9860bd9eb5cdb0fac848ed2ce08047
Andrea Escolar <andrea.escolar.artigas@ibadia.cat>
2026-03-16 13:00:25 +0100

    Añade estado para la provincia seleccionada

--- src/components/ProvinceTownSelector.tsx
+++ src/components/ProvinceTownSelector.tsx
@@ -1,17 +1,23 @@
+import { useState } from "react"
 import arbol from "../data/arbol.json"
 import type { AutonomousCommunity } from "../types/location"
 
 export default function ProvinceTownSelector () {
 
+    const [selectedProvince, setSelectedProvince] = useState<string>("")
+
     const data = arbol as AutonomousCommunity[]
     const provinces = data.flatMap((community) => community.provinces)
     // console.log(data)
-    console.log(provinces)
+    // console.log(provinces)
 
+    function handleProvinceChange(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSelectedProvince(event.target.value)
+    }
     return (
         <section>
             <label htmlFor="province">Provincia</label>
-            <select id="province">
+            <select id="province" value={selectedProvince} onChange={handleProvinceChange}>
                 <option value=""> Escoge una provincia</option>
 
                 {provinces.map((province) =>
