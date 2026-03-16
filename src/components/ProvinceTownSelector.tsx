663d19b2f1ba02b1486e831cfec526e62d8f34dd
Andrea Escolar <andrea.escolar.artigas@ibadia.cat>
2026-03-16 12:40:05 +0100

    Pinta el selector de provincias

--- src/components/ProvinceTownSelector.tsx
+++ src/components/ProvinceTownSelector.tsx
@@ -1,8 +1,25 @@
+import arbol from "../data/arbol.json"
+import type { AutonomousCommunity } from "../types/location"
 
 export default function ProvinceTownSelector () {
+
+    const data = arbol as AutonomousCommunity[]
+    const provinces = data.flatMap((community) => community.provinces)
+    // console.log(data)
+    console.log(provinces)
+
     return (
         <section>
-            <p>Aquí va selector provincias y ciudad</p>
+            <label htmlFor="province">Provincia</label>
+            <select id="province">
+                <option value=""> Escoge una provincia</option>
+
+                {provinces.map((province) =>
+                    <option key={province.code} value={province.label}>
+                        {province.label}
+                    </option>
+                )}
+            </select>
         </section>
     )
 } 
